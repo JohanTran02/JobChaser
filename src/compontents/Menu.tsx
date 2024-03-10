831081d075daf5bf8dd0b73168f517c3ad91e69f
JohanTran <johan.tran0210@gmail.com>
2024-03-11 00:27:47 +0100

    layout done

---
+++ src/compontents/Menu.tsx
@@ -0,0 +1,11 @@
+export function Menu() {
+    return (
+        <>
+            <div className="w-4/6 pt-10 m-auto">
+                <input type="search" className="w-full bg-gray-400 placeholder-black rounded-sm p-2" placeholder="Search a job..." />
+            </div>
+        </>
+    );
+}
+
+export default Menu;
---
+++ src/job.d.tsx
@@ -0,0 +1,13 @@
+export type Job = {
+    id?: string,
+    company: string,
+    logo: string,
+    position: string,
+    role?: string,
+    level?: string,
+    postedAt: string,
+    contract: string,
+    location: string,
+    languages?: string[],
+    tools?: string[]
+}
