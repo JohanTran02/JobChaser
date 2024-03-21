829c3683ca4faf56ee621d4ef1b1ff1baa1ff65d
JohanTran <johan.tran0210@gmail.com>
2024-03-21 09:36:43 +0100

    remvoed navigate on jobs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ export function NavBar() {
             <nav className="flex">
                 <ul>
                     <li><Link to="/JobChaser/">Home</Link></li>
-                    {/* <li><Link to="/JobChaser/Jobs">Jobs</Link></li> */}
+                    <li><Link to="/JobChaser/Jobs">Jobs</Link></li>
                     <li><Link to="/JobChaser/SignUp">Sign Up</Link></li>
                     <li><Link to="/JobChaser/SignIn">Sign in</Link></li>
                 </ul>
